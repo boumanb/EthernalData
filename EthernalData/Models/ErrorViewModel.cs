df26a89499963eccef1ab68451e003cefd69e4f6
BBouman <Billybouman@ziggo.nl>
2018-03-27 13:09:55 +0200

    Add project files.

---
+++ EthernalData/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EthernalData.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
