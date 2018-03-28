39c5a51bb4991ed85125e146eb93532d00f3fb42
BBouman <Billybouman@ziggo.nl>
2018-03-28 20:50:37 +0200

    Register with ETHAddress now possible

--- EthernalData/Controllers/AccountController.cs
+++ EthernalData/Controllers/AccountController.cs
@@ -220,7 +220,7 @@ namespace EthernalData.Controllers
             ViewData["ReturnUrl"] = returnUrl;
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, ETHAddress = model.ETHAddress };
                 var result = await _userManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
--- EthernalData/Data/Migrations/20180328183534_EthaddressAdded.Designer.cs
+++ EthernalData/Data/Migrations/20180328183534_EthaddressAdded.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace EthernalData.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180328183534_EthaddressAdded")]
+    partial class EthaddressAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
