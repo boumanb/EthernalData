86ae3ea55402f4b805d3fa51a4795242f29188f8
tijn167 <tijnvandenbergh2@hotmail.com>
2018-03-28 16:39:42 +0200

    Fixed the converter, gives back the base64 String for the image

--- EthernalData/Domain/Transaction.cs
+++ EthernalData/Domain/Transaction.cs
@@ -11,30 +11,23 @@ namespace EthernalData.Domain
         public int blockNumber;
         public string input;
 
-        public static byte[] HexStringToHex(string input)
+        public static byte[] StringToByteArray(string hex)
         {
-            var resultArray = new byte[input.Length / 2];
-            try
-            {
-
-                for (var i = 0; i < resultArray.Length; i++)
-                {
-                    resultArray[i] = System.Convert.ToByte(input.Substring(i * 2, 2), 16);
-                }
-            }
-            catch (System.FormatException ex)
-            {
-                Debug.WriteLine(ex.Message);
-            }
-            return resultArray;
+            string newHex = hex.Remove(0, 2);
+            Debug.WriteLine(newHex);
+            return Enumerable.Range(0, newHex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(newHex.Substring(x, 2), 16))
+                             .ToArray();
         }
 
         public string HexString2B64String(string input)
         {
             string result = "";
+        
             try
             {
-                result = System.Convert.ToBase64String(HexStringToHex(input));
+                result = System.Convert.ToBase64String(StringToByteArray(input));
 
             }
             catch (System.FormatException x)
