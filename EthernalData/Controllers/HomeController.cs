29b45a0b2889a1e2f572ace321d5a715b0b81c77
tijn167 <tijnvandenbergh2@hotmail.com>
2018-04-06 17:17:05 +0200

    Added changes to image overview design

--- EthernalData/Controllers/HomeController.cs
+++ EthernalData/Controllers/HomeController.cs
@@ -33,7 +33,7 @@ namespace EthernalData.Controllers
             _userManager = userManager;
         }       
 
-        [AllowAnonymous]
+        [Authorize]
         public IActionResult Index()
         {
             return View();
